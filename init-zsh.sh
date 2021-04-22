0f54980567d8512b2eefcb6f86dc5ff7c550146a
Ayoub Nasr <ayoub.nasr@scality.com>
2021-04-22 11:06:37 +0200

    initial zshrc

---
+++ init-zsh.sh
@@ -0,0 +1,56 @@
+# start starship shell prompt
+eval "$(starship init zsh)"
+
+# Source manjaro-zsh-configuration
+if [[ -e /usr/share/zsh/manjaro-zsh-config ]]; then
+  source /usr/share/zsh/manjaro-zsh-config
+fi
+
+# Use manjaro zsh prompt
+if [[ -e /usr/share/zsh/manjaro-zsh-prompt ]]; then
+  source /usr/share/zsh/manjaro-zsh-prompt
+fi
+
+if [[ -e ~/repos/ohmyzsh/lib/directories.zsh ]]; then
+  source ~/repos/ohmyzsh/lib/directories.zsh
+fi
+
+if [[ -e ~/repos/ohmyzsh/plugins/git/git.plugin.zsh ]]; then
+  if [[ -e ~/repos/ohmyzsh/lib/git.zsh ]]; then
+    source ~/repos/ohmyzsh/lib/git.zsh
+  fi
+  source ~/repos/ohmyzsh/plugins/git/git.plugin.zsh
+fi
+
+
+# change default browser
+export BROWSER='/usr/bin/google-chrome-stable'
+
+# vim as manpager
+export MANPAGER="env MAN_PN=1 vim -M +MANPAGER -"
+
+# repos
+: > ~/.repo_list
+find ~/repos -name .git -type d -prune | while read d; do
+   cd $d/..
+   echo "$PWD" >> ~/.repo_list
+   cd $OLDPWD
+done
+
+
+# some other aliases
+alias _='sudo'
+alias _p='sudo pacman'
+
+
+alias birth='cd ~/repos/thebirth'
+alias xssh='TERM=xterm ssh'
+alias hgrep="history 0 | ag"
+alias notes="swallow notepadqq ~/notepad.md"
+alias evimrc="vim ~/.vimrc"
+alias egitconfig="vim .gitconfig"
+alias ezshrc="vim ~/.zshrc"
+alias ei3rc="vim ~/.i3/config"
+alias eicons="vim ~/.i3/app-icons.json"
+alias gfpush='git push --force-with-lease --set-upstream origin $(git_current_branch)'
+alias _scratch="i3-msg move scratchpad"
