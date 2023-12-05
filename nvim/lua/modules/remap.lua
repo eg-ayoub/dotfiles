1a44d7b4e6f5dc5e1fd6848e23cb1c4e4708bbeb
Ayoub Nasr <ayoub.nasr@scality.com>
2023-12-05 11:17:40 +0100

    vim remaps

--- nvim/lua/modules/remap.lua
+++ nvim/lua/modules/remap.lua
@@ -5,12 +5,13 @@ vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 
 -- telescope
-local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
-vim.keymap.set('n', '<C-p>', builtin.git_files, {})
+local ts_builtin = require('telescope.builtin')
+vim.keymap.set('n', '<leader>pf', ts_builtin.find_files, {})
+vim.keymap.set('n', '<C-p>', ts_builtin.git_files, {})
 vim.keymap.set('n', '<leader>ps', function()
-    builtin.grep_string({ search = vim.fn.input("Grep >") })
+    ts_builtin.grep_string({ search = vim.fn.input("Grep >") })
 end)
+vim.keymap.set('n', '<leader>vh', ts_builtin.help_tags, {})
 
 -- undotree
 vim.keymap.set("n", "<leader>u", vim.cmd.UndotreeToggle)
@@ -26,3 +27,43 @@ lsp_zero.on_attach(function(client, bufnr)
     -- to learn the available actions
     lsp_zero.default_keymaps({ buffer = bufnr })
 end)
+
+-- move up and down
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
+-- append line
+vim.keymap.set("n", "J", "mzJ`z")
+
+-- half page jumps
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
+
+-- search terms stay in the middle
+vim.keymap.set("n", "n", "nzzzv")
+vim.keymap.set("n", "N", "Nzzzv")
+
+-- paste - don't replace yanked content
+vim.keymap.set("x", "<leader>p", [["_dP]])
+
+-- yank to xclip
+vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]])
+vim.keymap.set("n", "<leader>Y", [["+Y]])
+
+-- delete without yanking
+vim.keymap.set({ "n", "v" }, "<leader>d", [["_d]])
+
+-- format
+vim.keymap.set("n", "<leader>f", vim.lsp.buf.format)
+
+-- quick fix nav
+vim.keymap.set("n", "<C-k>", "<cmd>cnext<CR>zz")
+vim.keymap.set("n", "<C-j>", "<cmd>cprev<CR>zz")
+vim.keymap.set("n", "<leader>k", "<cmd>lnext<CR>zz")
+vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
+
+-- seek and replace
+vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
+
+-- make exec
+vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
