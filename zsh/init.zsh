fc583965a308411a680a3b45db91a7c560efce26
Ayoub Nasr <ayoub.nasr@scality.com>
2024-01-15 10:08:39 +0100

    split and worktree commands in zsh

--- zsh/init.zsh
+++ zsh/init.zsh
@@ -15,6 +15,21 @@ alias gfpush="ggpush --force"
 alias lssh="TERM=xterm ssh -F ssh_config"
 eval "$(starship init zsh)"
 
+function spl () { for x in {1..$1}; do alacritty & sleep 0.2 && disown ; done  }
+
+function fail () { echo $1 && return 1 }
+
+function worktree () {
+    setopt local_options err_return
+    git rev-parse --is-inside-work-tree >/dev/null 2>&1 || fail "Not in a git directory"
+    [[ -z $1 ]] && fail "Please specify worktree name"
+    reponame="$(basename $(git remote get-url origin))"
+    [[ -z "$reponame" ]] && fail "failed to fetch repository name"
+    treeloc="$HOME/repos/worktrees/$reponame/$1"
+    git worktree add -B "$1" "$treeloc" "$(git_current_branch)"
+    cd $treeloc
+}
+
 path+=('/home/ayoub/.local/bin')
 
 export PATH
