421dae53cb326da8cc6400b71161da068c65d012
Ayoub Nasr <ayoub.nasr@scality.com>
2024-01-16 20:24:13 +0100

    nvim install colorizer

--- nvim/lua/modules/look.lua
+++ nvim/lua/modules/look.lua
@@ -2,3 +2,6 @@ vim.cmd[[colorscheme tokyonight-moon]]
 
 vim.api.nvim_set_hl(0, "Normal", { bg = "none"})
 vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none"})
+
+-- setup colorizer
+require('colorizer').setup()
