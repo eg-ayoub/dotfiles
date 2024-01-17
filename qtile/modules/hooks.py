25b3f2705775fe27716c2805cb23d0ff27882edd
Ayoub Nasr <ayoub.nasr@scality.com>
2024-01-17 16:31:21 +0100

    better hooks

--- qtile/modules/hooks.py
+++ qtile/modules/hooks.py
@@ -1,27 +1,25 @@
 """Hooks for Qtile."""
+import time
 
-from libqtile import hook
+from libqtile import hook, qtile
 from libqtile.utils import send_notification
 from libqtile.log_utils import logger
 
-from .utils import run_local
+from .utils import run_local, run
 
 @hook.subscribe.screen_change
 def reconfigure_on_randr(_):
     """Run when screens are plugged/unplugged."""
-    logger.info("Reconfigure Qtile on screen_change")
     send_notification("Reconfiguring", "Reconfiguring Qtile due to screen change")
 
 @hook.subscribe.restart
 def restart():
     """Runs when qtile restarts."""
-    logger.info("Restarting Qtile")
     send_notification("Qtile restart", "Qtile is restarting")
     run_local(script="pre_start.sh")
 
-@hook.subscribe.startup
+@hook.subscribe.startup_complete
 def autostart():
     """Run when qtile starts or restarts."""
-    logger.info("Started Qtile")
     send_notification("Qtile started", "Qtile has started")
     run_local(script="post_start.sh")
